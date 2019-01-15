a483f58246c82987adf0cdc4f54099d0dda641ce
dr1zzt2 <dr1zzt2.NK@gmail.com>
2019-01-15 14:06:45 +0100

    added route for finding a project by id

--- src/main/java/com/codecool/contributor/controller/ProjectController.java
+++ src/main/java/com/codecool/contributor/controller/ProjectController.java
@@ -18,4 +18,11 @@ public class ProjectController {
     public List<Project> projectList() {
         return projectStorage.getAll().collect(Collectors.toList());
     }
+
+    @GetMapping(value="/project/{id}", produces = "application/json; charset=UTF-8")
+    public Project projectById(@PathVariable("id") Integer id) {
+        return projectStorage.find(id);
+    }
+
+
 }
