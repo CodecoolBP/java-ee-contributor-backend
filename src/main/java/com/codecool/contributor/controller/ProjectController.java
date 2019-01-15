322ed659486b7eecda6e002ffbefad8d5ebfc09d
dr1zzt2 <dr1zzt2.NK@gmail.com>
2019-01-15 14:47:12 +0100

    new routes for adding and deleting projects

--- src/main/java/com/codecool/contributor/controller/ProjectController.java
+++ src/main/java/com/codecool/contributor/controller/ProjectController.java
@@ -4,6 +4,8 @@ import com.codecool.contributor.model.Project;
 import com.codecool.contributor.service.implementation.ProjectStorageMem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,15 +16,25 @@ public class ProjectController {
     @Autowired
     private ProjectStorageMem projectStorage;
 
-    @GetMapping(value="/list", produces = "application/json; charset=UTF-8")
+    @GetMapping(value="/list")
     public List<Project> projectList() {
         return projectStorage.getAll().collect(Collectors.toList());
     }
 
-    @GetMapping(value="/project/{id}", produces = "application/json; charset=UTF-8")
+    @GetMapping(value="/project/{id}")
     public Project projectById(@PathVariable("id") Integer id) {
         return projectStorage.find(id);
     }
 
+    @PostMapping("/project/add")
+    public Project addProject(@RequestBody @Valid Project project) {
+        this.projectStorage.add(project);
+        return project;
+    }
 
+    @DeleteMapping(value="/project/{id}/delete")
+    public String deleteProjectById(@PathVariable("id") Integer id) {
+        this.projectStorage.remove(id);
+        return "Success.";
+    }
 }
