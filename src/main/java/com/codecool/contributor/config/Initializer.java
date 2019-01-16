59b0c938d165ae7e668569c81ee4d130d945c588
dr1zzt2 <dr1zzt2.NK@gmail.com>
2019-01-16 12:48:18 +0100

    changes to initializer

--- src/main/java/com/codecool/contributor/config/Initializer.java
+++ src/main/java/com/codecool/contributor/config/Initializer.java
@@ -20,9 +20,12 @@ public class Initializer {
         ArrayList<String> tags = new ArrayList<>();
         tags.add("tag1");
         tags.add("tag2");
+        ArrayList<String> tags2 = new ArrayList<>();
 
         projectStorage.add(new Project("ProjectCook", "Cook some stuff!", "Igen", tags, EnumStatus.valueOf("CLOSED")));
         projectStorage.add(new Project("ProjectBoi", "Boiboiboi!", "Nem", tags, EnumStatus.valueOf("OPEN")));
+        tags2.add("taglalat");
+        projectStorage.add(new Project("ProjectTagTest", "tagtest!", "Nem", tags2, EnumStatus.valueOf("OPEN")));
 
     }
 }
