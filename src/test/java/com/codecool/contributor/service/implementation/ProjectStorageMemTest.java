2d62e7c2fc080ad90aa3573045c6ec10035a2ae3
Szilárd <bszilard91@gmail.com>
2019-01-16 13:25:41 +0100

    Test ProjectStorageMemTest methods (add, find, remove).

---
+++ src/test/java/com/codecool/contributor/service/implementation/ProjectStorageMemTest.java
@@ -0,0 +1,54 @@
+package com.codecool.contributor.service.implementation;
+
+import com.codecool.contributor.config.Initializer;
+import com.codecool.contributor.model.EnumStatus;
+import com.codecool.contributor.model.Project;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.*;
+
+public class ProjectStorageMemTest {
+
+
+    ProjectStorageMem projectStorageMem = new ProjectStorageMem();
+    ArrayList<String> tags = new ArrayList<>();
+
+    @Before
+    public void init() throws Exception {
+
+        tags.add("tag1");
+        tags.add("tag2");
+        projectStorageMem.add(new Project("Name1", "Test some stuff!", "Igen", tags, EnumStatus.valueOf("CLOSED")));
+        projectStorageMem.add(new Project("Name2", "Test more stiff!", "Nem", tags, EnumStatus.valueOf("OPEN")));
+    }
+
+    @Test
+    public void testAddNewProject() {
+        projectStorageMem.add(new Project("Name3", "Test add method", "Nem", tags, EnumStatus.valueOf("OPEN")));
+        List result = projectStorageMem.getAll().collect(Collectors.toList());
+        assertEquals("Add new project testing", 3, result.size());
+    }
+
+    @Test
+    public void findProjectById() {
+        List excepted = projectStorageMem.getAll().collect(Collectors.toList());
+        Project result = projectStorageMem.find(1);
+        assertEquals("Find project by id testing", excepted.get(0), result);
+    }
+
+    @Test
+    public void removeProjectById() {
+        projectStorageMem.find(1);
+        projectStorageMem.remove(1);
+        assertNull("Remove project by id testing", projectStorageMem.find(1));
+    }
+}
