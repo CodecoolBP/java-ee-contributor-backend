98e3c275b3174460346e41616d69d3eb75e527db
dr1zzt2 <dr1zzt2.NK@gmail.com>
2019-01-30 09:52:07 +0100

    huge refactor for getting back inheritance

--- src/main/java/com/codecool/contributor/config/Initializer.java
+++ src/main/java/com/codecool/contributor/config/Initializer.java
@@ -35,46 +35,41 @@ public class Initializer {
             Project vöröskereszt = Project.builder()
                     .title("Learn first aid - app")
                     .shortDesc("Refresh your knowledge in first aid")
-                    .desc("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+                    .description("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
                     .organisation("Vöröskereszt")
                     .requirements("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
-                    .status(Status.OPEN)
                     .build();
 
             Project pharmap = Project.builder()
                     .title("Pharmacy map")
                     .shortDesc("A simple map application that shows you on a map the nearest pharmacys for you. You can add different addresses and a length in Km to the farest pharmacy. You can save locations, your and the pharmacy's locations as well")
-                    .desc("A simple map application with the nearest pharmacys")
+                    .description("A simple map application with the nearest pharmacys")
                     .organisation("Pharmap")
                     .requirements("Map API (Google Maps and Apple Maps for example.) User login to save locations")
-                    .status(Status.OPEN)
                     .build();
 
             Project doggo = Project.builder()
                     .title("Animal shelter web app")
                     .shortDesc("A web application for an animal shelter.")
-                    .desc("Web application for an animal shelter for an easier adoption process. We'd like a list of the animals with a datapage for each of them. And also we'd like a Map on our page for the soon to be adopters to find us easily")
+                    .description("Web application for an animal shelter for an easier adoption process. We'd like a list of the animals with a datapage for each of them. And also we'd like a Map on our page for the soon to be adopters to find us easily")
                     .organisation("Doggo")
                     .requirements("User registration, list of animals, datapage for animals, contact menu, map")
-                    .status(Status.OPEN)
                     .build();
 
             Project suitup = Project.builder()
                     .title("Clothes collecting app")
                     .shortDesc("Clothes collecting places mobile app")
-                    .desc("A simple mobile app (for Android and iOS too) that lists all the clothes collecting places with the type of clothes they need (adult, little girl or boy clothes etc.) The app would show the place's contacts, addresses and shows them on a map as well. It also sends you notification about the upcomming clothes collecting events.")
+                    .description("A simple mobile app (for Android and iOS too) that lists all the clothes collecting places with the type of clothes they need (adult, little girl or boy clothes etc.) The app would show the place's contacts, addresses and shows them on a map as well. It also sends you notification about the upcomming clothes collecting events.")
                     .organisation("Suit Up")
                     .requirements("List of clothes collecting places, Each of the places their own: contact page, address displayed on a map and clothes needs.")
-                    .status(Status.OPEN)
                     .build();
 
             Project childrenshope = Project.builder()
                     .title("Children's home donation page")
                     .shortDesc( "A simple donating page")
-                    .desc("A simple donation page for a children's home. You can send donations, check previous donating people and you can also read about the organisation's history and work")
+                    .description("A simple donation page for a children's home. You can send donations, check previous donating people and you can also read about the organisation's history and work")
                     .organisation("Children's hope")
                     .requirements("Donateing page, bank API, Organisation bio page, Previous donators page")
-                    .status(Status.OPEN)
                     .build();
 
             User user1 = User.builder()
--- src/main/java/com/codecool/contributor/model/BaseModel.java
+++ src/main/java/com/codecool/contributor/model/BaseModel.java
@@ -3,18 +3,27 @@ package com.codecool.contributor.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
 
 @NoArgsConstructor
 @Data
 public class BaseModel {
+
+    @Id
+    @GeneratedValue
     protected Integer id;
-    @NotEmpty
-    protected String name;
+
+    @Column(nullable = false)
+    protected String title;
+
+    @Column(nullable = false)
     protected String description;
 
-    BaseModel(String name, String description) {
-        this.name = name;
+    public BaseModel(String title, String description) {
+        this.title = title;
         this.description = description;
     }
 }
