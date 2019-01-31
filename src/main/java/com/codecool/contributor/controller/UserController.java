a3e85e15d430651565f4cc0fa6ccbedaf9df833d
dr1zzt2 <dr1zzt2.NK@gmail.com>
2019-01-31 10:19:24 +0100

    added UserController with routes for adding, finding and deleting

--- src/main/java/com/codecool/contributor/controller/UserController.java
+++ src/main/java/com/codecool/contributor/controller/UserController.java
@@ -1,11 +1,32 @@
 package com.codecool.contributor.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.codecool.contributor.entity.User;
+import com.codecool.contributor.service.UserStorage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
 @RequestMapping(value={"/api", "/"})
 public class UserController {
+
+    @Autowired
+    private UserStorage userStorage;
+
+    @GetMapping(value="/user/{id}")
+    public User userById(@PathVariable("id") Integer id) {
+        return userStorage.find(id);
+    }
+
+    @PostMapping("/user/add")
+    public User addUser(@RequestBody User user) {
+        this.userStorage.add(user);
+        return user;
+    }
+
+    @DeleteMapping(value="/user/{id}")
+    public String deleteUserById(@PathVariable("id") Integer id) {
+        this.userStorage.remove(id);
+        return "Success.";
+    }
 }
