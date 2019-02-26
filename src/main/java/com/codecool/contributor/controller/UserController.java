a41e5b212279290721fd09a103ff717fe655b727
dr1zzt2 <dr1zzt2.NK@gmail.com>
2019-02-26 16:04:30 +0100

    adding users now works with auth0

--- src/main/java/com/codecool/contributor/controller/UserController.java
+++ src/main/java/com/codecool/contributor/controller/UserController.java
@@ -2,8 +2,18 @@ package com.codecool.contributor.controller;
 
 import com.codecool.contributor.model.User;
 import com.codecool.contributor.service.UserStorage;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.jwt.Jwt;
+import org.springframework.security.jwt.JwtHelper;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.HashMap;
 
 @RestController
 @CrossOrigin
@@ -13,15 +23,31 @@ public class UserController {
     @Autowired
     private UserStorage userStorage;
 
+    @Autowired
+    private HttpServletRequest request;
+
     @GetMapping(value="/user/{id}")
     public User userById(@PathVariable("id") Integer id) {
         return userStorage.find(id);
     }
 
     @PostMapping("/user/add")
-    public User addUser(@RequestBody User user) {
-        this.userStorage.add(user);
-        return user;
+    public String addUser(@RequestBody String idToken) throws IOException {
+        idToken = idToken.replaceAll("^\"|\"$", "");
+        Jwt decodedToken = JwtHelper.decode(idToken);
+        String jwtClaims = decodedToken.getClaims();
+        HashMap claimsMap = new ObjectMapper().readValue(jwtClaims, HashMap.class);
+        Object userEmail = claimsMap.get("email");
+        User newUser = User.builder()
+                .email(userEmail.toString())
+                .build();
+        try {
+            userStorage.add(newUser);
+        }
+        catch (DataIntegrityViolationException e) {
+            System.out.println("User already in database.");
+        }
+        return "Success.";
     }
 
     @DeleteMapping(value="/user/{id}")
